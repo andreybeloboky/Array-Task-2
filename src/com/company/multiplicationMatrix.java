63c9dfda379b0f14fc6a7499a1ea8d18bf4a3bb5
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-07-16 16:28:43 +0300

    4 commit

--- src/com/company/multiplicationMatrix.java
+++ src/com/company/multiplicationMatrix.java
@@ -5,8 +5,8 @@ public class multiplicationMatrix {
         int[][] multiplicationMatrix = new int[numberOfColumnsMatrixA][numberOfLineMatrixB];
         for (int i = 0; i < numberOfColumnsMatrixA; i++) {
             for (int j = 0; j < numberOfLineMatrixB; j++) {
-                for (int k = 0; k < numberOfLineMatrixA; k++) {
-                    multiplicationMatrix[i][j] = matrixA[i][k] * matrixB[k][j];
+                for (int k = 1; k < numberOfLineMatrixA; k++) {
+                    multiplicationMatrix[i][j] = matrixA[i][j] * matrixB[i][k] + matrixA[i][k] * matrixB[k][j];
                 }
             }
         }
