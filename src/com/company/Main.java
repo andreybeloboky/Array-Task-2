7be4e1822cd315ccd6f20ddef5f6a1b9915c9255
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-07-19 12:16:19 +0300

    delete .idea

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,24 +11,27 @@ public class Main {
         int numberOfLineMatrixA = scanner.nextInt();
         System.out.println("Enter number of columns: ");
         int numberOfColumnsMatrixA = scanner.nextInt();
-        System.out.println("Enter number of line: ");
-        int[][] matrixA = Matrix.creatureMatrix(numberOfLineMatrixA, numberOfColumnsMatrixA);
         System.out.println("Enter size matrixA");
         System.out.println("Enter number of line: ");
         int numberOfLineMatrixB = scanner.nextInt();
         System.out.println("Enter number of columns: ");
         int numberOfColumnsMatrixB = scanner.nextInt();
-        int[][] matrixB = Matrix.creatureMatrix(numberOfLineMatrixB, numberOfColumnsMatrixB);
-        if (numberOfColumnsMatrixA == numberOfLineMatrixB) {
-            int[][] matrixAB = MultiplicationMatrix.calculatorMatrix(matrixA, matrixB);
-            for (int i = 0; i < numberOfLineMatrixA; i++) {
-                for (int j = 0; j < numberOfColumnsMatrixB; j++) {
-                    System.out.print(" " + matrixAB[i][j] + " ");
+        if (numberOfColumnsMatrixA >= 0 && numberOfLineMatrixA >= 0 && numberOfColumnsMatrixB >= 0 && numberOfLineMatrixB >= 0) {
+            int[][] matrixA = Matrix.creatureMatrix(numberOfLineMatrixA, numberOfColumnsMatrixA);
+            int[][] matrixB = Matrix.creatureMatrix(numberOfLineMatrixB, numberOfColumnsMatrixB);
+            if (numberOfColumnsMatrixA == numberOfLineMatrixB) {
+                int[][] matrixAB = MultiplicationMatrix.calculatorMatrix(matrixA, matrixB);
+                for (int i = 0; i < numberOfLineMatrixA; i++) {
+                    for (int j = 0; j < numberOfColumnsMatrixB; j++) {
+                        System.out.print(" " + matrixAB[i][j] + " ");
+                    }
+                    System.out.println();
                 }
-                System.out.println();
+            } else {
+                System.out.println("Does not meet the matrix multiplication condition");
             }
         } else {
-            System.out.println("Does not meet the matrix multiplication condition");
+            System.out.println("Matrix size must be greater than 0");
         }
     }
 }
